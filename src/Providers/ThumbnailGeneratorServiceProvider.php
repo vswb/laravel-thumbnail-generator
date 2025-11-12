a05887fe753c4c3207f597153a149c654df5a3aa
Eugene Lee|apikey:1f6239abe7e9bca321dda9a3ec3d53cfbc840f279d2a029d6601ae733ccf2ec4 <eugene-macbook@fsofts.com>
2025-11-12 18:20:24 +0700

    no message

--- src/Providers/ThumbnailGeneratorServiceProvider.php
+++ src/Providers/ThumbnailGeneratorServiceProvider.php
@@ -45,27 +45,27 @@ class ThumbnailGeneratorServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // $this
-        //     ->setNamespace('packages/thumbnail-generator')
-        //     ->loadRoutes()
-        //     ->loadAndPublishTranslations()
-        //     ->loadMigrations()
-        //     ->loadAndPublishViews()
-        //     ->loadHelpers()
-        // ;
+        $this
+            ->setNamespace('packages/thumbnail-generator')
+            ->loadRoutes()
+            ->loadAndPublishTranslations()
+            ->loadMigrations()
+            ->loadAndPublishViews()
+            ->loadHelpers()
+        ;
 
         // Ensure core helpers are loaded before using add_filter
-        // $this->app->booted(function () {
-        //     // Define constant if not exists (for compatibility)
-        //     if (!defined('BASE_FILTER_AFTER_SETTING_CONTENT')) {
-        //         define('BASE_FILTER_AFTER_SETTING_CONTENT', 'base_filter_after_setting_content');
-        //     }
+        $this->app->booted(function () {
+            // Define constant if not exists (for compatibility)
+            if (!defined('BASE_FILTER_AFTER_SETTING_CONTENT')) {
+                define('BASE_FILTER_AFTER_SETTING_CONTENT', 'base_filter_after_setting_content');
+            }
 
-        //     if (function_exists('add_filter')) {
-        //         add_filter('handle_filter_value_maxsize', [$this, 'handleSetMaxFileSize'], 10, 2);
-        //         add_filter(BASE_FILTER_AFTER_SETTING_CONTENT, [$this, 'renderSetting'], 10, 1);
-        //     }
-        // });
+            if (function_exists('add_filter')) {
+                add_filter('handle_filter_value_maxsize', [$this, 'handleSetMaxFileSize'], 10, 2);
+                add_filter(BASE_FILTER_AFTER_SETTING_CONTENT, [$this, 'renderSetting'], 10, 1);
+            }
+        });
     }
 
     public function fileSizeConvert($size)
